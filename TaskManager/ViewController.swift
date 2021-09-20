6c408e515663b3824ad2426f60b4413735f02065
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-20 13:33:32 +0300

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Василий Пронин on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
